8b9e5199675d066ef834c4f815345dd3e41070c5
Carolina Mascarenhas <carolina.mascarenhass96@hotmail.com>
2022-02-04 13:18:44 -0200

    requisito 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,68 @@
 import React from 'react';
+import './components/components.css';
 import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: 'normal',
+      cardTrunfo: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { name, value, type, checked } }) {
+    if (type === 'checkbox') {
+      return this.setState({
+        [name]: checked,
+      });
+    }
+    return this.setState({
+      [name]: value,
+    });
+  }
+
   render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.state;
     return (
-      <div>
+      <div className="components">
         <Form
-          cardName=""
-          cardDescription=""
-          cardAttr1=""
-          cardAttr2=""
-          cardAttr3=""
-          cardImage=""
-          cardRare=""
-          cardTrunfo=""
-          hasTrunfo=""
-          isSaveButtonDisabled=""
-          onInputChange=""
-          onSaveButtonClick=""
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
+          onInputChange={ this.handleChange }
         />
         <Card
-          cardName=""
-          cardDescription=""
-          cardAttr1=""
-          cardAttr2=""
-          cardAttr3=""
-          cardImage=""
-          cardRare=""
-          cardTrunfo=""
+          cardName={ cardName }
+          cardDescription={ cardDescription }
+          cardAttr1={ cardAttr1 }
+          cardAttr2={ cardAttr2 }
+          cardAttr3={ cardAttr3 }
+          cardImage={ cardImage }
+          cardRare={ cardRare }
+          cardTrunfo={ cardTrunfo }
         />
       </div>
     );
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -10,10 +10,11 @@ class Card extends React.Component {
       cardAttr3,
       cardImage,
       cardRare,
-      cardTrunfo } = this.props;
+      cardTrunfo,
+    } = this.props;
 
     return (
-      <>
+      <div>
         <p data-testid="name-card">{cardName}</p>
         <img src={ cardImage } alt={ cardName } data-testid="image-card" />
         <p data-testid="description-card">{cardDescription}</p>
@@ -21,8 +22,9 @@ class Card extends React.Component {
         <p data-testid="attr2-card">{cardAttr2}</p>
         <p data-testid="attr3-card">{cardAttr3}</p>
         <p data-testid="rare-card">{cardRare}</p>
-        { cardTrunfo ? <p data-testid="trunfo-card">Super Trunfo</p> : '' }
-      </>
+        { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
+        {/* vi no code review do fechamento e achei melhor trocar o ternario pelo && */}
+      </div>
     );
   }
 }
